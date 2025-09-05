e90e228ee1c793ef03644678a742a43b2ea6d24e
Stanislaw <stasgolovnev22@gmail.com>
2025-09-05 14:56:04 +0300

    Upgrade actions

--- src/test/java/plugins/allure/AllureUtils.java
+++ src/test/java/plugins/allure/AllureUtils.java
@@ -1,13 +1,29 @@
 package plugins.allure;
 
+import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class AllureUtils {
-    @Attachment(value = "screenshot", type = "image/png")
-    public static byte[] takeScreenshot(WebDriver driver) {
-        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+
+    public static void takeScreenshot(WebDriver driver) {
+        try {
+            // Проверяем, что тест запущен
+            if (Allure.getLifecycle().getCurrentTestCase().isPresent()) {
+                byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                Allure.getLifecycle().addAttachment(
+                        "Screenshot",
+                        "image/png",
+                        "png",
+                        screenshot
+                );
+            } else {
+                System.out.println("WARN: Cannot take screenshot - no test is running");
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to take screenshot: " + e.getMessage());
+        }
     }
 }
